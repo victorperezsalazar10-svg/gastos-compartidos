2377f0d53528adbd980765f1ec2c1316a843d046
Víctor Pérez <victorperezsalazar10@gmail.com>
2026-06-10 12:25:02 -0500

    Primer commit - Estructura inicial del proyecto Gastos Compartidos

---
+++ gastos-compartidos/src/test/java/com/gastos/gastos_compartidos/GastosCompartidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gastos.gastos_compartidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastosCompartidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
